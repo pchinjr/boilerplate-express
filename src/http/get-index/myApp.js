0fd1e115132e5294069832b5146ff99fecf2a91d
Paul Chin Jr <pchinjr@gmail.com>
2020-07-01 15:46:12 -0400

    step 6 - env var uppercase

--- src/http/get-index/myApp.js
+++ src/http/get-index/myApp.js
@@ -31,7 +31,11 @@ app.use('/', express.static(__dirname + "/public"))
 /** 5) serve JSON on a specific route */
 
 app.get('/json', function(req, res) {
-  res.json({"message":"Hello json"})
+  if(process.env.MESSAGE_STYLE) {
+    res.json({"message":"HELLO JSON"})
+  } else {
+    res.json({"message":"Hello json"})
+  }
 })
 
 /** 6) Use the .env file to configure the app */
