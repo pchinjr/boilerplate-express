30116044c5943a9443120e6e90da7c780f473682
Paul Chin Jr <pchinjr@gmail.com>
2020-07-01 14:53:05 -0400

    step 3

--- src/http/get-index/myApp.js
+++ src/http/get-index/myApp.js
@@ -10,15 +10,19 @@ var app = express();
 
 /** 1) Meet the node console. */
 
-console.log("Hello World")
+//console.log("Hello World")
 
 /** 2) A first working Express Server */
-app.get('/', function(req, res) {
-  res.send('Hello Express')
-})
+// app.get('/', function(req, res) {
+//   res.send('Hello Express')
+// })
 
 /** 3) Serve an HTML file */
 
+app.get('/', function(req,res){
+  let absolutePath = __dirname + "/views/index.html"
+  res.sendFile(absolutePath)
+})
 
 /** 4) Serve static assets  */
 
